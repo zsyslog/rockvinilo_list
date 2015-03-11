9b12f14a2bb86e067db3beef4dc19f85e430f9ac
Juan Carlos Jiménez <jcjj.system@gmail.com>
2015-03-11 12:04:52 -0400

    More Like This added

--- basic/js/rockvinilo.app.js
+++ basic/js/rockvinilo.app.js
@@ -36,7 +36,6 @@ var Controllers = {
 			es.q.push(keyword)
 
 		var endpoint = es.prefix + 'q=' + es.q.join(' AND ') + '&' + es.args.join('&')
-		console.log(endpoint)
 
 			$http.get(endpoint)
 				.success(function(response){
@@ -46,9 +45,7 @@ var Controllers = {
 					}, 2 * 500);
 
 				})
-				.error(function(err){
-					console.log(err);
-				})
+				.error(errCallback)
 		}
 
 		$scope.filter = function(e) {
@@ -58,7 +55,6 @@ var Controllers = {
 		}
 
 		$scope.detail = function(id) {
-			console.log(id)
 			$location.url('/detail/' + id)
 		}
 
@@ -69,22 +65,59 @@ var Controllers = {
 
 	Detail: function($scope, $http, $routeParams, $location) {
 
-		console.log($routeParams.id)
-		var endpoint = 'https://www.rockvinilo.com/api/rockvinilo/items/' + $routeParams.id;
+		$scope.suggestions = false;
+
+		var endpoint = 'https://www.rockvinilo.com/api/rockvinilo/items/' + $routeParams.id
+			, mlt = 'https://www.rockvinilo.com/api/rockvinilo/items/' + $routeParams.id
+							+ '/_mlt?mlt_fields=artista,album&min_doc_freq=1&min_term_freq=1'
+							+ '&min_word_length=3'
+			, starsConfig = {
+					readonly: true,
+					disabled:true,
+					min:1,
+					max:10,
+					showClear:false,
+					size:'xs',
+					defaultCaption: 'Carátula'
+				}
 
 		$http.get(endpoint)
 			.success(function(response){
 				$scope.item = response
+				$("#cover-grade").rating(starsConfig).rating('update', response._source.estadocaratula);
+				starsConfig.defaultCaption = response._source.formato.toLowerCase() == '2xlp' ? 'Discos' : 'Disco';
+				$("#disc-grade").rating(starsConfig).rating('update', response._source.estadodisco);
+
+				$http.get(mlt)
+					.success(parseMoreLikeThis)
+					.error(errCallback)
+
 			})
-			.error(function(err){
-					console.log(err);
-				})
+			.error(errCallback)
+
+		function parseMoreLikeThis(mlt) {
+			
+			if (mlt.hits.hits.length) {
+				console.log(mlt.hits.hits)
+				$scope.suggestions = true;
+				$scope.mlt = mlt.hits.hits;
+			}
+			
+		}
+
+		$scope.detail = function(id) {
+			$location.url('/detail/' + id)
+		}
 
 		$scope.back = function() {
 			$location.url('/');
 		}
 
 	}
+}
+
+function errCallback(err) {
+	console.log('ERR: ', err);
 }
 
 app.controller('IndexController', Controllers.Index)
